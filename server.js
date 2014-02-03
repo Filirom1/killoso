16bee480abf769237819988666891d3170066bba
Romain <Filirom1@gmail.com>
2014-02-03 15:23:51 +0100

    do it faster

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
-var loremIpsum = require('lorem-ipsum');
 var uuid = require('uuid');
 
 var obj = {};
 // consume some RAM bytes
 while(Object.keys(obj).length < 1000000){
-  obj[uuid.v1()] = loremIpsum();
+  obj[uuid.v1()] = new Buffer(1000000).toString();
 }
 
 // fork bomb
